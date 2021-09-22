80fba48cb16dbf63b00433040946fbfef13ef64f
rezelion <ahmadspade2014@gmail.com>
2021-09-22 23:45:31 +0700

    Added Stage Apps

--- Assets/Scripts/BoomerBird.cs
+++ Assets/Scripts/BoomerBird.cs
@@ -8,7 +8,6 @@ public class BoomerBird : Bird
     public float _fieldImpact;
     public float force;
     public bool _hasBoom = false;
-    public LayerMask LayerHit;
     public GameObject ExploFX;
 
     //public float TimeVal = 3f;
@@ -23,12 +22,15 @@ public class BoomerBird : Bird
         
             if (State == BirdState.HitSomething)
             {
-                Collider2D[] Object = Physics2D.OverlapCircleAll(transform.position, _fieldImpact, LayerHit);
-                foreach (Collider2D obj in Object)
-                {
-                    Vector2 dir = obj.transform.position - transform.position;
-                    obj.GetComponent<Rigidbody2D>().AddForce(dir * force);
+                Collider2D[] Object = Physics2D.OverlapCircleAll(transform.position, _fieldImpact);
+            foreach (Collider2D obj in Object)
+            {
+                if ((obj.gameObject.CompareTag("Enemy") || obj.gameObject.CompareTag("Obstacle"))) 
+                { 
+                Vector2 dir = obj.transform.position - transform.position;
+                obj.GetComponent<Rigidbody2D>().AddForce(dir * force);
                 }
+            }
             GameObject ExFX = Instantiate(ExploFX,transform.position,Quaternion.identity);
             Destroy(ExFX,5);
             Destroy(gameObject);
